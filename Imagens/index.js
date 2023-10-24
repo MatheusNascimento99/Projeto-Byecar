987c1a799721ef5bad1390fdc608ada4eeaeee95
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-10-24 19:41:39 -0300

    first commit

---
+++ Imagens/index.js
@@ -0,0 +1,15 @@
+var card = window.document.getElementById('card')
+var icon1 = window.document.getElementById('icon1')
+var icon2 = window.document.getElementById('icon2')
+
+function Volts (){
+    card.src = '/volts.svg'
+    icon1.setAttribute("onclick", "Bandit()")
+    icon2.setAttribute("onclick", "Bandit()")
+}
+
+function Bandit (){
+    card.src = '/bandit.svg'
+    icon1.setAttribute("onclick", "Volts()")
+    icon2.setAttribute("onclick", "Volts()")
+}
